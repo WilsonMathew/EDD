6812687c344c1a76009993e7df4a879744799db5
Wilson <wilsonmathewone@gmail.com>
2020-06-05 00:19:27 -0600

    couple of menu improvements

--- EDD/main.cpp
+++ EDD/main.cpp
@@ -10,21 +10,24 @@ using namespace std;
 #define arrayLen(ar) (sizeof(ar)/sizeof(ar[0])) 
 
 // Menus to select one option
-void menu(string menuData[], int menuSize ) 
+int menu(string menuData[], int menuSize ) 
 {
+	int opcion;
 	// element 0 of the array will define the current menu;
-	cout << "*          " << menuData[0]   << "				*\n";
+	cout << "*" << menuData[0]   << " *\n\n";
 
+	// show menu text
 	for (int i = 1; i < menuSize; i++)
 	{
-		cout << "-> " << menuData[i] << ": \n";
+		cout << "-> " << i << ". " << menuData[i] << "\n";
 	}
 	
+	cin >> opcion;
 
-	// fill the user info
+	return opcion;
 }
 
-
+// Login Menu using user test
 void loginMenu()
 {
 	user test;
@@ -49,8 +52,19 @@ void loginMenu()
 
 int main()
 {
-	string test[] = {"Login","Ingresar usuario","Ingresar contraseña", "Ingresar Departamento","Ingresar Empresa"};
-	//menu(test, arrayLen(test));
+	// Menu logged in user
+	string test[] = {"$$$USERNAME$$$",
+		"Agregar activo", 
+		"Eliminar activo",
+		"Modificar activo",
+		"Rentar activo",
+		"Activos rentados",
+		"Mis activos rentados",
+		"Cerrar sesion"};
 	loginMenu();
+	system("CLS");
+	int n = menu(test,arrayLen(test));
+
+
 	return 0;
 }
